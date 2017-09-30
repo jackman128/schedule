5aa0cb03c1ea4c771b7a8c4a9f92a89093b309b9
Jack Wilson <jackwilson128@gmail.com>
2017-09-30 13:42:17 -0700

    Switched back to the old optimization method (SectionsForTerm). Also added a bunch of comments and added clarity.

--- DvcSchedule5.cpp
+++ DvcSchedule5.cpp
@@ -4,35 +4,34 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <functional>
 
 using namespace std;
 
 #include <cstring>
 #include "DynamicArray.h"
 
-const string fileName = "dvc-schedule.txt";
-
-struct Course
-{
-  short term, section;
-};
-
-struct CodeSectionPair
+//This struct holds the number of sections offered for each subject.
+struct SectionsForSubject
 {
   string subjectCode;
   int numSections = 0;
 };
 
+//This struct holds an array of section numbers that correspond to a term.
 struct SectionsForTerm
 {
   int sectionsSeen = 0;
-  DynamicArray<Course> seenSectionNumber = DynamicArray<Course>(8);
+  //section codes are always 4 digits, so 'short' should be big enough.
+  DynamicArray<short> seenSectionNumber;
 };
 
-short termToShort(char *str);
+//This function maps a term string ("Fall 2007") to an index for 'alreadySeen'.
+//It does this because jumping to a location in alreadySeen is much faster
+//than doing a linear search for a term string.
+size_t termToIndex(const string &str);
 
-const int NUM_BUCKETS = 256;
+//This is the name of the input file.
+const string fileName = "dvc-schedule.txt";
 
 int main()
 {
@@ -42,31 +41,31 @@ int main()
   cout << "File: " << __FILE__ << endl;
   cout << endl;
 
-  char* token;
-  char buf[1000];
-  const char* const tab = "\t";
 
-  //open the input file
+  //open the input file and throw an exception if it's unsuccessful
   ifstream fin;
   fin.open(fileName);
   if (!fin.good()) throw "I/O error";
 
-  //'courses' will hold all of the courses that have been processed so far
-  //subjectCodes will hold the subject codes seen so far
-  //and the number of courses for each subject.
-  //CodeSectionPair subjectCodes[200];
+  //subjectCodes: holds the number of sections for each subject.
+  DynamicArray<SectionsForSubject> subjectCodes(150);
 
-  SectionsForTerm seen[NUM_BUCKETS];
+  //alreadySeen: holds arrays full of section numbers for each term
+  //in order to keep track of duplicates.
+  DynamicArray<SectionsForTerm> alreadySeen(100);
 
-  hash<int> hasher;
-
+  //numSubjectCodes: number of unique subject codes, like "MATH" or "ENGL"
   int numSubjectCodes = 0;
-  int numCourses = 0;
+  //duplicates: number of duplicates counted so far
   int duplicates= 0;
+  //line: the current line being parsed from the input file
   string line;
-  getline(fin, line);
+  //token: points to a position in buf
+  char* token;
+  //buf: holds a portion of the input file for parsing
+  char buf[1000];
+  const char* const tab = "\t";
   while (fin.good()) {
-    Course c;
     // read the line
     getline(fin, line);
     strcpy(buf, line.c_str());
@@ -74,31 +73,42 @@ int main()
     if (buf[0] == 0) continue; // skip blank lines
 
     //parse the line
-    c.term = termToShort(token = strtok(buf, tab));
-    c.section = atoi((token = strtok(0, tab)) ? token : "");
-    //string course((token = strtok(0, tab)) ? token : "");
-    //string instructor((token = strtok(0, tab)) ? token : "");
-    //string whenWhere((token = strtok(0, tab)) ? token : "");
-    //if (course.find('-') == string::npos) continue; // invalid line: no dash in course name
-    //string subjectCode(course.begin(), course.begin() + course.find('-'));
+    const string term(token = strtok(buf, tab));
+    const string section((token = strtok(0, tab)) ? token : "");
+    const string course((token = strtok(0, tab)) ? token : "");
+    const string instructor((token = strtok(0, tab)) ? token : "");
+    const string whenWhere((token = strtok(0, tab)) ? token : "");
+    if (course.find('-') == string::npos) continue; // invalid line: no dash in course name
+    const string subjectCode(course.begin(), course.begin() + course.find('-'));
 
-    //Dedup
+
+    //DEDUP
     bool isDup = false;
-    unsigned hash = hasher(c.section + c.term) % NUM_BUCKETS;
-    for (int i = 0; i < seen[hash].sectionsSeen; i++) {
-      if (c.term == seen[hash].seenSectionNumber[i].term && c.section == seen[hash].seenSectionNumber[i].section) {
+    size_t index = termToIndex(term); //convert term to an index in sectionsSeen
+    short sectionShort = static_cast<short>(stoi(section)); //convert section to a short
+    //loop over section numbers in the corresponding SectionsForTerm in 'alreadySeen'
+    //until a duplicate is found
+    for (int i = 0; i < alreadySeen[index].sectionsSeen; i++) {
+      if (sectionShort == alreadySeen[index].seenSectionNumber[i]) {
         isDup = true;
         duplicates++;
         break;
       }
     }
-    if (!isDup) {
-      seen[hash].seenSectionNumber[seen[hash].sectionsSeen++] = c;
+    //If it's a duplicate, skip to the next line in the file.
+    if (isDup) {
+      continue;
     }
-    /*
-    //count
-    //If a new subject code is found, add it to subjectCodes
-    //and increment numSubjectCodes
+    else {
+      //If it's not a duplicate, insert this section number
+      //into the corresponding SectionsForTerm.
+      int i = alreadySeen[index].sectionsSeen++;
+      alreadySeen[index].seenSectionNumber[i] = sectionShort;
+    }
+
+    //COUNT
+    //Find the matching subject code for the course that is currently being looked at
+    //and increment the counter for that subject code.
     int j = 0;
     for (; j < numSubjectCodes; j++) {
       if (subjectCodes[j].subjectCode == subjectCode) {
@@ -107,23 +117,22 @@ int main()
         break;
       }
     }
-    //if the end of the previous loop was reached, then this is a new subject code.
+    //if the end of the previous loop was reached, then this is a subject code
+    //that hasn't been seen before. Insert it into subjectCodes and increment
+    //numSubjectCodes.
     if (j == numSubjectCodes) {
       subjectCodes[j].subjectCode = subjectCode;
       subjectCodes[j].numSections++;
       numSubjectCodes++;
     }
-    //add this course to the courses array and increment the number of courses.
-    numCourses++;
-    */
   }
+  //Close the file, it's not being read anymore.
   fin.close();
 
-  //insertion sort subjectCodes by subjectCode in alphabetical order
-  /*
+  //Use insertion sort to sort the subject codes into alphabetical order.
   int i = 1;
   while (i < numSubjectCodes) {
-    CodeSectionPair t = subjectCodes[i];
+    SectionsForSubject t = subjectCodes[i];
     int j = i - 1;
     while (j >= 0 && subjectCodes[j].subjectCode > t.subjectCode) {
       subjectCodes[j + 1] = subjectCodes[j];
@@ -133,43 +142,36 @@ int main()
     i++;
   }
 
-  //Now print out the number of subject codes, the array of subject codes
-  //along with the number of sections
+  //Now print out the number of unique subject codes,
+  //each subject code with the number of sections,
+  //and the number of duplicates found.
   cout << endl;
   cout << "Subject codes: " << numSubjectCodes << endl;
   for (int i = 0; i < numSubjectCodes; i++) {
     cout << subjectCodes[i].subjectCode <<": " << subjectCodes[i].numSections << endl;
   }
   cout << endl;
-  */
   cout << "Duplicates: " << duplicates << endl;
 
   return 0;
 }
 
-short termToShort(char *str)
+size_t termToIndex(const string &str)
 {
-  /*
-  const char* const space = " ";
-  char  *tok;
-  char *seasonStr = (tok = strtok(str, space));
-  int year = atoi(tok);
+  //Each term string has a season (Fall, Spring, or Summer)
+  //and a year, separated by a space.
+  string seasonStr = str.substr(0, str.find(" "));
+  string year = str.substr(str.find(" "));
 
   int seasonNum;
-  if (seasonStr[1] == 'a')
+  if (seasonStr == "Fall")
     seasonNum = 0;
-  else if (seasonStr[1] == 'p')
+  else if (seasonStr == "Spring")
     seasonNum = 1;
   else
     seasonNum = 2;
-  return year * 3 - 6003 + seasonNum;
-  */
 
-  if (str[1] == 'a')
-    return atoi(str + 5) * 3 - 6003;
-  else if (str[1] == 'p')
-    return atoi(str + 7) * 3 - 6002;
-  else
-    return atoi(str + 7) * 3 - 6001;
-
+  //The year is multiplied by three to "make room" for the seasonNum.
+  //Subtract by 6003 to make "Fall 2001" the 0th index.
+  return stoi(year) * 3 + seasonNum - 6003;
 }
--- DynamicArray.h
+++ DynamicArray.h
@@ -6,122 +6,120 @@
 template <typename T>
 class DynamicArray
 {
-    private:
-        //points to the array
-        T* array;
-        //dummy value is returned on an invalid access
-        T dummy;
-        //cap is the capacity
-        int cap;
-
-    public:
-
-//main constructor: initializes values.
-//The default capacity is 2.
-DynamicArray(int cap = 2)
-{
-    //the value of dummy doesn't matter
-    this->dummy = T();
-    //set the capacity
-    this->cap = cap;
-    //allocate memory to hold the array.
-    this->array = new T[cap];
-    //set all values in the array to the default value
-    for (int i = 0; i < cap; i++) {
+ private:
+  //points to the array
+  T* array;
+  //dummy value is returned on an invalid access
+  T dummy;
+  //cap is the capacity
+  int cap;
+  
+ public:
+  
+  //main constructor: initializes values.
+  //The default capacity is 2.
+  DynamicArray(int cap = 2)
+    {
+      //the value of dummy doesn't matter
+      this->dummy = T();
+      //set the capacity
+      this->cap = cap;
+      //allocate memory to hold the array.
+      this->array = new T[cap];
+      //set all values in the array to the default value
+      for (int i = 0; i < cap; i++) {
         this->array[i] = T();
+      }
     }
-}
-
-//Copy constructor: copies all elements over to the
-//new object, except for dummy
-DynamicArray(const DynamicArray<T> &original)
-{
-    this->dummy = T();
-    this->cap = original.cap;
-    this->array = new T[original.cap];
+  
+  //Copy constructor: copies all elements over to the
+  //new object, except for dummy
+  DynamicArray(const DynamicArray<T> &original)
+    {
+      this->dummy = T();
+      this->cap = original.cap;
+      this->array = new T[original.cap];
     //copy contents of original array to the new one.
-    for (int i = 0; i < original.cap; i++) {
+      for (int i = 0; i < original.cap; i++) {
         this->array[i] = original.array[i];
+      }
     }
-}
-
-//assignment operator: If the two arrays are not the same,
-//then perform the same thing as the copy constructor, then return a reference.
-//otherwise, just return a reference to itself.
-DynamicArray<T> operator=(const DynamicArray<T> &original)
-{
-    if (this != &original) {
+  
+  //assignment operator: If the two arrays are not the same,
+  //then perform the same thing as the copy constructor, then return a reference.
+  //otherwise, just return a reference to itself.
+  DynamicArray<T> operator=(const DynamicArray<T> &original)
+    {
+      if (this != &original) {
         delete[] array;
-
+        
         this->dummy = T();
         this->cap = original.cap;
         this->array = new T[original.cap];
         for (int i = 0; i < original.cap; i++) {
-            this->array[i] = original.array[i];
+          this->array[i] = original.array[i];
         }
+      }
+      return *this;
     }
-    return *this;
-}
-
-//Destructor: delete the contents of the array
-~DynamicArray()
-{
-    delete[] array;
-}
-
-//Capacity getter: return the current capacity
-int Capacity() const
-{
+  
+  //Destructor: delete the contents of the array
+  ~DynamicArray()
+    {
+      delete[] array;
+    }
+  
+  //Capacity getter: return the current capacity
+  int Capacity() const
+  {
     return cap;
-}
-
-//Capacity setter: set the new capacity and move all array elements
-//to a new array with the proper size.
-void Capacity(int newCapacity)
-{
+  }
+  
+  //Capacity setter: set the new capacity and move all array elements
+  //to a new array with the proper size.
+  void Capacity(int newCapacity)
+  {
     T* newArray = new T[newCapacity];
-
+    
     //in the first loop, go from zero to the lesser of newCapacity or cap.
     //this ensures that we stay within the bounds of the original array.
     int loopLength = (newCapacity < this->cap) ? (newCapacity) : (this->cap);
     for (int i = 0; i < loopLength; i++) {
-        newArray[i] = this->array[i];
+      newArray[i] = this->array[i];
     }
     for (int i = loopLength; i < newCapacity; i++) {
-        newArray[i] = T();
+      newArray[i] = T();
     }
     delete[] array;
     array = newArray;
     cap = newCapacity;
-}
-
-//operator[] getter: return the element indicated.
-//If the index is invalid, return dummy
-T operator[](int index) const
-{
+  }
+  
+  //operator[] getter: return the element indicated.
+  //If the index is invalid, return dummy
+  T operator[](int index) const
+  {
     if (index < 0 || index >= cap)
-        return dummy;
+      return dummy;
     return array[index];
-}
-
-//operator[] setter: return a reference to the element indicated.
-//If the index is negative, return dummy.
-//If the index is greater than the current capacity, adjust the capacity
-//and then return a reference to the indicated element.
-T& operator[](int index)
-{
-    if (index < 0) {
+  }
+  
+  //operator[] setter: return a reference to the element indicated.
+  //If the index is negative, return dummy.
+  //If the index is greater than the current capacity, adjust the capacity
+  //and then return a reference to the indicated element.
+  T& operator[](int index)
+    {
+      if (index < 0) {
         return dummy;
-    }
-    if (index >= cap) {
+      }
+      if (index >= cap) {
         Capacity(index * 2);
         return array[index];
+      }
+      return array[index];
     }
-    return array[index];
-}
-
+  
 };
-
-
 
 #endif // DYNAMICARRAY_H_INCLUDED
